2b2329f81ed6b752e6d2b2a5dfd0f8284342dfa8
YubelFTAE <32706401+YubelFTAE@users.noreply.github.com>
2018-10-28 16:08:57 +0700

    Login
    demo

---
+++ FastJobs/app/src/main/java/com/example/fastjobs/View/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.fastjobs.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.fastjobs.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void Login(View v){
+
+    }
+}
