696095f869dd767e37acd59fc6645906bdfe7579
vanthai2704 <vanthai2704@gmail.com>
2018-10-29 09:18:45 +0700

    merge code

--- FastJobs/app/src/main/java/com/example/fastjobs/MainActivity.java
+++ FastJobs/app/src/main/java/com/example/fastjobs/MainActivity.java
@@ -1,20 +1,71 @@
 package com.example.fastjobs;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.transition.FragmentTransitionSupport;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.example.fastjobs.firebase.LoginSupport;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        Intent intentTest = new Intent(this, TestActivity.class);
-        startActivity(intentTest);
+        drawerLayout = findViewById(R.id.drawer);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setNavigationViewListner();
+
+    }
+
+
+    private void setNavigationViewListner() {
+        navigationView = findViewById(R.id.design_navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        android.support.v4.app.FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                ft.replace(R.id.drawer,new HomeFragment());
+                ft.commit();
+                navigationView.setCheckedItem(R.id.home);
+                break;
+            case R.id.addPost:
+                Toast.makeText(this, "addPost", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search:
+                ft.replace(R.id.drawer,new SearchFragment());
+                ft.commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
