38452a5ddb9d84b2e807754876ec4514852d3ee8
vanthai2704 <vanthai2704@gmail.com>
2018-10-29 09:15:14 +0700

    Commit menu

---
+++ FastJobs/app/src/main/java/com/example/fastjobs/HomeFragment.java
@@ -0,0 +1,30 @@
+package com.example.fastjobs;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends android.support.v4.app.Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        getActivity().setTitle("Home Fragment");
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
