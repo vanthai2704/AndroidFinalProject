25bb8e0044d5badd2f8a34c1d8b6a09a36edfb79
ngquangdat <ngquangdat13@gmail.com>
2018-10-30 13:10:54 +0700

    Fix conflict

---
+++ FastJobs/app/src/main/java/com/example/fastjobs/view/LoginActivity.java
@@ -0,0 +1,98 @@
+package com.example.fastjobs.View;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fastjobs.MainActivity;
+import com.example.fastjobs.R;
+import com.example.fastjobs.firebase.LoginSupport;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.tooltip.Tooltip;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+
+    static final String validemail = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +"\\@" +"[a-zA-Z0-9]" +
+            "[a-zA-Z0-9\\-]{0,64}" +"(" +"\\." +"[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +")+";
+    private TextView username,password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+    }
+
+    public void Login(View v) throws InterruptedException {
+
+
+        String email = username.getText().toString();
+        String pass = password.getText().toString();
+
+        showTooltip(email,pass);
+
+    }
+    public void Register(View v){
+        Toast.makeText(getApplicationContext(),"Register",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void showTooltip (String tooltipmail,String tooltippass) throws InterruptedException {
+
+        Matcher matcher= Pattern.compile(validemail).matcher(tooltipmail);
+
+
+        if(tooltipmail.equals("") || !matcher.matches()){
+            Tooltip tooltip = new Tooltip.Builder(username).setText("InValid Email")
+                    .setTextColor(Color.RED)
+                    .setGravity(Gravity.TOP)
+                    .setCornerRadius(8f)
+                    .setDismissOnClick(true)
+                    .show();
+            //Thread.sleep(3000);
+            //tooltip.wait(2000);
+            //tooltip.dismiss();
+            return;
+        }
+        if(tooltippass.equals("")){
+            Tooltip tooltip = new Tooltip.Builder(password).setText("InValid Password")
+                    .setTextColor(Color.RED)
+                    .setGravity(Gravity.TOP)
+                    .setCornerRadius(8f)
+                    .setDismissOnClick(true)
+                    .show();
+            //Thread.sleep(3000);
+            //tooltip.dismiss();
+            return;
+        }
+
+
+        if (matcher.matches()){
+            LoginSupport loginSupport = new LoginSupport();
+            Task<AuthResult> task = loginSupport.login(tooltipmail,tooltippass);
+            if(task.isSuccessful()){
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"Email or Password is InValid",Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+}
---
+++ FastJobs/app/src/main/java/com/example/fastjobs/view/RegisterActivity.java
@@ -0,0 +1,61 @@
+package com.example.fastjobs.View;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fastjobs.MainActivity;
+import com.example.fastjobs.R;
+import com.example.fastjobs.firebase.LoginSupport;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView email, pass, repass;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        email = findViewById(R.id.editText_enter_username);
+        pass = findViewById(R.id.editText_enter_password);
+        repass = findViewById(R.id.editText_enter_rpassword);
+        register = findViewById(R.id.button_register);
+    }
+
+    public void Register(View v){
+        String getemail = email.getText().toString();
+        String getpass = pass.getText().toString();
+        String getrepass = repass.getText().toString();
+
+        Matcher matcher= Pattern.compile(LoginActivity.validemail).matcher(getemail);
+        if(getemail.equals("") || !matcher.matches()){
+            Toast.makeText(getApplicationContext(),"InValid Email",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(getpass.equals("")){
+            Toast.makeText(getApplicationContext(),"Require Password",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(!getpass.equals(getrepass)){
+            Toast.makeText(getApplicationContext(),"Repeat Password and Password not be the same",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        if (matcher.matches()){
+            LoginSupport loginSupport = new LoginSupport();
+            loginSupport.signUp(getemail,getpass);
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
